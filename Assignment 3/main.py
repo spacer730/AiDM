3f44d291a2e3e4074712a2a9a0a8e163531959bb
spacer730 <hoeshashemi_199@msn.com>
2018-10-16 17:15:52 +0200

    Mostly done

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -3,6 +3,7 @@ import numpy as np
 import timeit
 import random
 import math
+import itertools
 
 np.random.seed(42)
 
@@ -20,7 +21,8 @@ row = np.array(um_data[:,1]) #Array of the movie_id's of each rating
 col = np.array(um_data[:,0]) #Array of the user_id's of each rating
 data = np.ones(len(um_data)) #The rated movie's will be saved as a one
 
-#binary_matrix = sparse.coo_matrix((data, (row,col)))#, shape=(row.max()+1,col.max()+1)).toarray()
+local_binary_matrix = sparse.coo_matrix((data, (row,col)))#, shape=(row.max()+1,col.max()+1)).toarray()
+local_binary_matrix = local_binary_matrix.tocsr()
 
 """
 def make_sign_matrix(binary_matrix[:,0], num_signs):
@@ -38,7 +40,7 @@ def make_sign_matrix(binary_matrix[:,0], num_signs):
 
 def make_sign_matrix(row, col, data, num_signs):
   local_binary_matrix = sparse.coo_matrix((data, (row,col)))
-  local_binary_matrix = binary_matrix.tocsr()
+  local_binary_matrix = local_binary_matrix.tocsr()
   sign_matrix = np.zeros((num_signs,col.max()+1))
   
   for sign in range(num_signs):
@@ -49,30 +51,66 @@ def make_sign_matrix(row, col, data, num_signs):
 
   return sign_matrix
 
-sign_matrix = make_sign_matrix(row, col, data, 50)
-
-def LSH(sign_matrix, b, r, k):
-  buckets = np.zeros(k)
-  req_buckets = math.floor(sign_matrix[:,0]/b)
-
-  
-  
+def LSH(sign_matrix, b, r):
+  num_sign = len(sign_matrix[:,0])
+  req_buckets = int(math.floor(num_sign/r))
+  candidate_pairs = []
   for band in range(req_buckets):
     buckets = {}
     band_indices = range(band*r,(1+band)*r)
     for user_id in range(len(sign_matrix[0])):
-      h = hash(sign_matrix[band_indices,user_id].tolist())
-      buckets['s']=buckets['s'].append(223)
+      h = hash(tuple(sign_matrix[band_indices,user_id]))
       if h in buckets:
-        buckets.[h]
-    
-  
-  for band in range(b):
-    band_indices = np.random.
+        buckets[h].append(user_id)
+      else:
+        buckets[h]=[user_id]
+    for bucket, user_ids in buckets.items():
+      if len(user_ids)>1:
+        for pair in itertools.combinations(user_ids,2):
+          candidate_pairs.append(pair)
+    print(band)
+  for band in range(req_buckets,b):
+    buckets = {}
+    band_indices = random.sample(range(num_sign), r)
     for user_id in range(len(sign_matrix[0])):
-      hash.
-
-
+      h = hash(tuple(sign_matrix[band_indices,user_id]))
+      if h in buckets:
+        buckets[h].append(user_id)
+      else:
+        buckets[h]=[user_id]
+    for bucket, user_ids in buckets.items():
+      if len(user_ids)>1:
+        for pair in itertools.combinations(user_ids,2):
+          candidate_pairs.append(pair)
+    print(band)
   return candidate_pairs
 
-candidate_pairs = LSH(sign_matrix, 5, 10,
+def jaccard_similarity(user_1, user_2):
+   s1 = set(local_binary_matrix[:,user_1].nonzero()[0])
+   s2 = set(local_binary_matrix[:,user_2].nonzero()[0])
+   intersection = s1.intersection(s2)
+   union = s1.union(s2)
+   return float(len(intersection)/len(union))
+
+def controle(pairs):
+   finalpairs = []
+   for i in range(500):#len(pairs)):
+      if jaccard_similarity(int(pairs[i][0]), int(pairs[i][1])) > 0.5:
+         finalpairs.append(pairs[i])
+      print(i)
+   return finalpairs
+
+"""
+def controle2(pairs):
+  finalpairs = []
+  for i in range(len(pairs)):
+    if jaccard_similarity_score(local_binary_matrix[:,pairs[i][0]], local_binary_matrix[:,pairs[i][1]]) > 0.5:
+      finalpairs.append(pairs[i])
+    print(i)
+  return finalpairs
+"""
+
+sign_matrix = make_sign_matrix(row, col, data, 50)
+candidate_pairs = LSH(sign_matrix, 5, 11)
+uniq_candidate_pairs = np.unique(candidate_pairs, axis=0)
+final_pairs = controle(uniq_candidate_pairs)
